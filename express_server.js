a9af67c1bc92937e8fe07b4a6c7611b69b49612f
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-11-28 19:20:13 -0500

    Added new route definition

--- express_server.js
+++ express_server.js
@@ -21,6 +21,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const userInput = req.params.id
   const templateVars = { id: userInput, longURL: urlDatabase[userInput] };
